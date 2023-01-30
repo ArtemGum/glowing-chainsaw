df2d4c18d3bf2447d892e08ab6d369c0d3a80483
Gumarov Artem <artemgumarov840@gmail.com>
2023-01-30 23:55:53 +0300

    add config

---
+++ config/build/types/config.ts
@@ -0,0 +1,19 @@
+import webpack from "webpack";
+
+export interface BuildPaths {
+    entry: string
+    build: string
+    html: string
+}
+
+export interface BuildEnv {
+    mode: webpack.Configuration['mode']
+    port: number
+}
+
+export interface BuildOptions {
+    mode: webpack.Configuration['mode']
+    paths: BuildPaths
+    isDev: boolean
+    port: number
+}
