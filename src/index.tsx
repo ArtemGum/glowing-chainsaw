17d0a594d3efbafbd258f6a09e44aff1d29d6b04
Artem Gumarov <gumarov.a@citilink.ru>
2023-02-23 17:22:54 +0300

    add redux store, add first entity

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { render } from 'react-dom';
-import App from 'app/App';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-
+import App from 'app/App';
 import 'app/styles/index.scss';
 import 'shared/config/i18n/i18n';
-import { StoreProvider } from 'app/providers/StoreProvider';
 
 render(
     <StoreProvider>
